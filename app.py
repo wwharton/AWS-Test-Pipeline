f26d870067082c1426d6fbd22297194c943383e5
wwharton <wwharton@umich.edu>
2021-01-13 16:45:15 -0500

    test commit, fixed missing AWS creds on GH secrets

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
-# Test Comment to push a change - Updated IAM role perms
+# Test Comment to push a change - Fixed Missing AWS creds
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
